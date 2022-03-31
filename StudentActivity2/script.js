29ace099851a73409ea43b2208d54bcd0c40d5e3
rbidon <roussebidon@gmail.com>
2022-03-30 22:56:50 -0400

    completed studentActivity part

---
+++ StudentActivity2/script.js
@@ -0,0 +1,12 @@
+// elemt 1
+let heading = document.querySelector("#heading");
+heading.setAttribute("style", "color:#FEB95F");
+// element 2
+let Flowerimg = document.querySelectorAll("img");
+// select all must indicate specific index I want to change
+Flowerimg[0].setAttribute("src","./playfulSloth.jpg");
+Flowerimg[0].setAttribute("style", "width:25%", "height:25%");
+//  element 3
+let table = document.querySelectorAll("table");
+table[0].setAttribute("style", "border:1px solid black;");
+
---
+++ studentActivity1/script.js
@@ -0,0 +1,12 @@
+// assigned value & grab the specific element from
+let heading = document.getElementById('header');
+// style the element
+heading.style.color = "#5CA4A9";
+
+// Element 2
+let fruitsalad = document.getElementById('fruits');
+fruitsalad.style.backgroundColor = "#E6EBE0";
+
+// element 3
+let activities = document.getElementById('activities');
+activites.style.color = "#C2095A";
